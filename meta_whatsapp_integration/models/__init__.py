aa652a50c021941e5f8ae317b3d4711aa60d68c0
gajumogare143 <114096700+gajumogare143@users.noreply.github.com>
2026-02-14 11:37:37 +0530

    Import multiple modules in __init__.py

--- meta_whatsapp_integration/models/__init__.py
+++ meta_whatsapp_integration/models/__init__.py
@@ -1,1 +1,7 @@
-
+from . import whatsapp_business_account
+from . import whatsapp_message
+from . import whatsapp_template
+from . import res_partner
+from . import pos_order
+from . import sale_order
+from . import purchase_order
